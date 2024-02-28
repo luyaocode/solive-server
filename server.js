6e28c79e856cfeb795357421691e71dacf9f37cd
luyaocode <316774040@qq.com>
2024-02-28 18:10:53 +0800

    修复开发模式问题

--- server.js
+++ server.js
@@ -213,24 +213,25 @@ function verifyToken(token, secretKey) {
 
 // Server
 const express = require('express');
+const http = require('http');
 const https = require('https');
 const app = express();
 const fs = require('fs');
 let ssl_crt, ssl_key;
+let server, options;
 if (process.env.NODE_ENV === 'prod') {
     ssl_crt = '/home/luyao/codes/chaos-gomoku/ssl/chaosgomoku.fun.pem';
     ssl_key = '/home/luyao/codes/chaos-gomoku/ssl/chaosgomoku.fun.key';
+    options = {
+        key: fs.readFileSync(ssl_key),
+        cert: fs.readFileSync(ssl_crt)
+    }
+    server = https.createServer(options, app);
 }
 else if (process.env.NODE_ENV === 'dev') {
-    ssl_crt = '/home/luyao/codes/chaos-gomoku-server/myserver/ssl/chaosgomoku.fun.pem';
-    ssl_key = '/home/luyao/codes/chaos-gomoku-server/myserver/ssl/chaosgomoku.fun.key';
-}
-const options = {
-    key: fs.readFileSync(ssl_key),
-    cert: fs.readFileSync(ssl_crt)
+    server = http.createServer(app);
 }
 
-const server = https.createServer(options, app);
 const socket = require('socket.io');
 const io = socket(server, {
     cors: {
