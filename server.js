9236c8719d61a6ad03adb6f92717313d2badb7aa
luyaocode <316774040@qq.com>
2024-03-14 12:32:42 +0800

    优化

--- server.js
+++ server.js
@@ -22,12 +22,23 @@ const GameMode = {
 const PublicMsg_Max_Length = 5000;
 const Notice_Max_Length = 1000;
 const TitleNotice = {
-    message: '富强、民主、文明、和谐。自由、平等、公正、法治。爱国、敬业、诚信、友善。',
+    message: '社会主义核心价值观：富强、民主、文明、和谐；自由、平等、公正、法治；爱国、敬业、诚信、友善。\
+    Core values of socialism: Prosperity, democracy, civilization, harmony; freedom, equality, justice, rule of law; patriotism, dedication, integrity, kindness.\
+    सोशलिज़म के मूल्यों: समृद्धि, लोकतंत्र, सभ्यता, सामंजस्य; स्वतंत्रता, समानता, न्याय, कानून का शासन; देशभक्ति, समर्पण, ईमानदारी, मित्रता।\
+    社会主義の核心価値観: 繁栄、民主主義、文明、調和; 自由、平等、正義、法の支配; 愛国心、献身、誠実、親切。\
+    Основные ценности социализма: процветание, демократия, цивилизация, гармония; свобода, равенство, справедливость, верховенство права; патриотизм, преданность, целостность, доброта.\
+    Valori fondamentali del socialismo: prosperità, democrazia, civiltà, armonia; libertà, uguaglianza, giustizia, stato di diritto; patriottismo, dedizione, integrità, gentilezza.\
+    Valeurs fondamentales du socialisme : prospérité, démocratie, civilisation, harmonie ; liberté, égalité, justice, État de droit ; patriotisme, dévouement, intégrité, gentillesse.\
+    Kernwerte des Sozialismus: Wohlstand, Demokratie, Zivilisation, Harmonie; Freiheit, Gleichheit, Gerechtigkeit, Rechtsstaatlichkeit; Patriotismus, Hingabe, Integrität, Freundlichkeit.\
+    사회주의의 핵심 가치: 번영, 민주주의, 문명, 조화; 자유, 평등, 정의, 법치; 애국심, 헌신, 정직, 친절.\
+    القيم الأساسية للإشتراكية: الازدهار، الديمقراطية، الحضارة، الانسجام؛ الحرية، المساواة، العدالة، سيادة القانون؛ الوطنية، التفاني، النزاهة، اللطف.\
+    Βασικές αξίες του σοσιαλισμού: ευημερία, δημοκρατία, πολιτισμός, αρμονία; ελευθερία, ισότητα, δικαιοσύνη, κράτος δικαίου; πατριωτισμός, αφοσίωση, ακεραιότητα, ευγένεια.',
     id: '小棋',
     timestamp: Date.now(),
     locationData: {
         country: '中国',
-        city: '湖北'
+        region: '湖北',
+        city: '广水',
     }
 }
 
