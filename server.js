f440fd52616ef7aed060123c794101af2a68757e
luyaocode <316774040@qq.com>
2024-03-17 16:11:12 +0800

    transceiverRequest

--- server.js
+++ server.js
@@ -474,6 +474,10 @@ function handleVideoChat(socket) {
     socket.on("stopShareScreen", (data) => {
         io.to(data.to).emit("stopShareScreen");
     });
+
+    socket.on("transceiverRequest", (data) => {
+        io.to(data.to).emit("transceiverRequest", data.signal);
+    });
 }
 
 function publishNotice(socket, noticeType, loc, roomId, nickName) {
