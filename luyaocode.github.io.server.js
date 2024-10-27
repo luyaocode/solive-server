2df62eb9e625bf45a047d491930fb49cce17cd80
luyaocode <316774040@qq.com>
2024-10-28 01:45:10 +0800

    add blog tags

--- luyaocode.github.io.server.js
+++ luyaocode.github.io.server.js
@@ -15,6 +15,22 @@ const initdb = () => {
         dialect: 'sqlite',
         storage: 'luyaocode.github.io.db' // 数据库文件路径
     });
+
+    // 定义标签模型
+    const Tag = sequelize.define('Tag', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        }
+    });
+
+    // 定义文章模型
     const Blog = sequelize.define('Blog',
         {
             id: {
@@ -50,29 +66,160 @@ const initdb = () => {
     Blog.beforeDestroy(async (instance, options) => {
         await instance.update({ updatedAt: new Date() }, { silent: true });
     });
-    return { sequelize, Blog }
+
+    // 定义多对多关联
+    const BlogTag = sequelize.define('BlogTag', {});
+
+    // 设置模型关联
+    Blog.belongsToMany(Tag, { through: BlogTag });
+    Tag.belongsToMany(Blog, { through: BlogTag });
+
+
+    // 同步数据库
+    // await sequelize.sync({ force: true }); // 清空数据库，慎用
+    sequelize.sync()
+    .then(() => {
+        console.log('数据库已同步');
+    })
+    .catch(err => {
+        console.error('同步失败:', err);
+    });
+
+    return { sequelize, Blog, Tag, BlogTag };
 }
 
-const rundb = async () => {
-    const { sequelize,Blog}=initdb();
-    try {
-        // await sequelize.sync({ force: true }); // 清空数据库
-        await sequelize.sync();
+/**
+ * 测试用
+ */
+const test = async () => {
+    async function printBlogsWithTags() {
+        const { Blog, Tag } = db; // 确保 db 中导入了 Blog 和 Tag 模型
 
-        logger.info("Database & table synced!");
-        const posts = await Blog.findAll();
-        logger.info(`All posts: ${JSON.stringify(posts, null, 2)}`);
-    } catch (error) {
-        logger.error('Error occurred:', error);
-    } finally {
-        sequelize.close().catch(error => logger.error('Error closing connection after failure:', error));;
+        try {
+            // 查询所有博客，并包括关联的标签
+            const blogs = await Blog.findAll({
+                include: [{
+                    model: Tag,
+                    through: { attributes: [] } // 只选择关联表的内容，忽略中间表的属性
+                }]
+            });
+
+            // 打印每个博客及其关联的标签
+            blogs.forEach(blog => {
+                console.log(`博客 ID: ${blog.id}, 标题: ${blog.title}, 作者: ${blog.author}, 时间: ${blog.time}`);
+                console.log('关联的标签:');
+                blog.Tags.forEach(tag => {
+                    console.log(`- ${tag.name}`);
+                });
+            });
+        } catch (error) {
+            console.error('查询失败:', error);
+        }
     }
-};
 
-const postBlog = async (uuid,title, content) => {
-    const { sequelize,Blog}=initdb();
+    // 执行函数
+    printBlogsWithTags();
+}
+
+const db = initdb(); // 单例
+
+// 标签操作
+// 创建标签
+createTag("teee");
+async function createTag(name) {
+    const { Tag } = db;
     try {
-        await sequelize.sync();
+        let tag = await Tag.findOne({ where: { name } });
+        if (tag) {
+            console.log(`标签已存在: ${tag.name}`);
+            return;
+        }
+        tag = await Tag.create({ name });
+        console.log(`标签已创建: ${tag.name}`);
+    } catch (error) {
+        console.error('创建标签失败:', error);
+    }
+}
+
+async function createTags(tags) {
+    if (tags) {
+        for (const tag of tags) {
+            await createTag(tag);
+        }
+    }
+}
+
+// 查询所有标签
+async function getAllTags() {
+    test();
+    const { Tag } = db;
+    try {
+        const tags = await Tag.findAll();
+        console.log('所有标签:', tags);
+        return tags;
+    } catch (error) {
+        console.error('查询标签失败:', error);
+    }
+}
+
+// 根据多个标签名查询对应id数组
+async function getTagIdsByNames(tagNames) {
+    const { Tag } = db;
+    try {
+        const tags = await Tag.findAll({
+            where: {
+                name: tagNames
+            },
+            attributes: ['id']
+        });
+
+        const tagIds = tags.map(tag => tag.id);
+        return tagIds;
+    } catch (error) {
+        console.error('Error fetching tag IDs:', error);
+        throw error;
+    }
+}
+
+// 更新标签
+async function updateTag(tagId, newName) {
+    const { Tag } = db;
+    try {
+        const tag = await Tag.findByPk(tagId);
+        if (tag) {
+            tag.name = newName;
+            await tag.save();
+            console.log(`标签已更新: ${tagId} -> ${newName}`);
+            return tag;
+        } else {
+            console.log(`未找到标签 ID: ${tagId}`);
+        }
+    } catch (error) {
+        console.error('更新标签失败:', error);
+    }
+}
+
+// 删除标签
+async function deleteTag(tagId) {
+    const { Tag } = db;
+    try {
+        const deletedCount = await Tag.destroy({
+            where: { id: tagId }
+        });
+        if (deletedCount > 0) {
+            console.log(`已删除标签 ID: ${tagId}`);
+        } else {
+            console.log(`未找到标签 ID: ${tagId}`);
+        }
+    } catch (error) {
+        console.error('删除标签失败:', error);
+    }
+}
+
+// 博客操作
+const postBlog = async (uuid, title, content,tags) => {
+    const { Blog } = db;
+    try {
         const newPost = await Blog.create({
             id: uuid,
             title: title,
@@ -80,18 +227,20 @@ const postBlog = async (uuid,title, content) => {
             content: content,
             time: new Date()
         });
+        await createTags(tags);
+        // 关联
+        const tagIds=await getTagIdsByNames(tags);
+        newPost.setTags(tagIds);
+
         logger.info(`New post created: ${JSON.stringify(newPost.toJSON(), null, 2)}`);
     } catch (error) {
         logger.error('Error occurred:', error);
-    } finally {
-        sequelize.close().catch(error => logger.error('Error closing connection after failure:', error));;
     }
 }
 
 const updateBlog = async (uuid, title, content) => {
-    const { sequelize, Blog } = initdb();
+    const { Blog } = db;
     try {
-        await sequelize.sync();
         const [updatedRows] = await Blog.update(
             {
                 title: title,
@@ -113,13 +262,12 @@ const updateBlog = async (uuid, title, content) => {
     } catch (error) {
         logger.error('Error occurred while updating blog:', error);
         return false;
-    } finally {
-        sequelize.close().catch(error => logger.error('Error closing connection after failure:', error));;
     }
 }
 
+// 查询所有博客，不带任何条件
 const getBlogs = async () => {
-    const { sequelize,Blog}=initdb();
+    const {Blog}=db;
     try {
         const blogs = await Blog.findAll({
             attributes: ['id', 'title', 'author', 'time'], // 选择需要的字段
@@ -137,13 +285,47 @@ const getBlogs = async () => {
         return res;
     } catch (error) {
         logger.error('Error occurred:', error);
-    } finally {
-        sequelize.close().catch(error => logger.error('Error closing connection after failure:', error));;
     }
 }
 
+// 查询博客，根据标签数组
+const getBlogsByTags = async (tags) => {
+    if (!tags) {
+        return await getBlogs();
+    }
+    const { Blog, Tag } = db;
+    try {
+        const blogs = await Blog.findAll({
+            attributes: ['id', 'title', 'author', 'time'], // 选择需要的字段
+            include: [{
+                model: Tag,
+                where: { id: tags }, // 使用 tagIds 查询符合条件的标签
+                through: { attributes: [] }
+            }],
+            order: [['time', 'DESC']], // 按时间倒序排序
+            logging: console.log // 记录 SQL 查询到控制台
+        });
+
+        const res = blogs.map(blog => {
+            logger.info(`Title: ${blog.title}, Author: ${blog.author}, Time: ${blog.time}`);
+            return {
+                id: blog.id,
+                title: blog.title,
+                author: blog.author,
+                time: blog.time,
+            };
+        });
+
+        return res;
+    } catch (error) {
+        console.error('查询错误:', error);
+        return [];
+    }
+};
+
+// 查询博客，根据id
 const getBlogById = async (id) => {
-    const { sequelize, Blog } = initdb();
+    const { Blog } = db;
     try {
         // 查找指定 id 的记录
         const blog = await Blog.findOne({
@@ -169,14 +351,42 @@ const getBlogById = async (id) => {
         }
     } catch (error) {
         logger.error('Error finding blog by id:', error);
-    } finally {
-        sequelize.close().catch(error => logger.error('Error closing connection after failure:', error));;
     }
 };
 
+// 查询博客，根据id数组
+const getBlogsByIds = async (ids) => {
+    const { Blog } = db;
+    try {
+        const blogs = await Blog.findAll({
+            attributes: ['id', 'title', 'author', 'time'], // 选择需要的字段
+            order: [['time', 'DESC']], // 按照时间字段倒序排序
+            ...(ids.length > 0 // 如果 blogIds 不为空，则根据其进行过滤
+                ? {
+                    where: {
+                        id: ids
+                    }
+                }
+                : {})
+        });
+        const res = blogs.map(blog => {
+            logger.info(`Title: ${blog.title}, Author: ${blog.author}, Time: ${blog.time}`);
+            return {
+                id: blog.id,
+                title: blog.title,
+                author: blog.author,
+                time: blog.time,
+            }
+        });
+        return res;
+    } catch (error) {
+        logger.error('Error finding blog by id:', error);
+    }
+}
+
 // 删除指定 ID 的记录
 async function deleteBlogById(id) {
-    const { sequelize, Blog } = initdb();
+    const { Blog } = db;
     try {
         const deletedRows = await Blog.destroy({
             where: {
@@ -186,37 +396,14 @@ async function deleteBlogById(id) {
         logger.info(`Deleted ${deletedRows} rows`);
     } catch (error) {
         logger.error('Error deleting blog:', error);
-    } finally {
-        sequelize.close().catch(error => logger.error('Error closing connection after failure:', error));;
     }
 }
-
-// const getBlogsLatestUpdateTime = async () => {
-//     const { sequelize, Blog } = initdb();
-//     try {
-//         const latestUpdate = await Blog.findOne({
-//             order: [['updatedAt', 'DESC']],
-//             attributes: ['updatedAt']
-//         });
-//         if (latestUpdate) {
-//             logger.info(`Latest update: ${latestUpdate.updatedAt}`);
-//             return latestUpdate.updatedAt;
-//         } else {
-//             return null;
-//         }
-//     } catch (error) {
-//         logger.error('Error deleting blog:', error);
-//     } finally {
-//         sequelize.close().catch(error => logger.error('Error closing connection after failure:', error));;
-//     }
-// }
-
 /**
  *
  * @returns 最新的修改记录（包括软删除、增加、修改）
  */
 const getBlogsLatestUpdateTime = async () => {
-    const { sequelize, Blog } = initdb();
+    const { Blog } = db;
     try {
         // 获取最新的更新时间记录
         const latestUpdate = await Blog.findOne({
@@ -251,18 +438,15 @@ const getBlogsLatestUpdateTime = async () => {
         }
     } catch (error) {
         logger.error('Error fetching the latest record:', error);
-    } finally {
-        sequelize.close().catch(error => logger.error('Error closing connection after failure:', error));;
     }
 };
-
 
 /**
  * 恢复所有软删除的记录
  * @returns None
  */
 async function restoreAllBlogs() {
-    const { sequelize, Blog } = initdb();
+    const { Blog } = db;
     try {
         const restoredRows = await Blog.restore({
             where: {}, // 不指定条件，恢复所有软删除记录
@@ -276,13 +460,8 @@ async function restoreAllBlogs() {
         }
     } catch (error) {
         logger.error('Error restoring blogs:', error);
-    } finally {
-        sequelize.close().catch(error => logger.error('Error closing connection after failure:', error));;
     }
 }
-
-rundb();
-//
 // restoreAllBlogs();
 
 import http from 'http';
@@ -340,7 +519,12 @@ const check = (pwd) => {
 // 处理 POST 请求的路由
 app.post('/publish', async (req, res) => {
     const { type, uuid, title, content } = req.body;
-    const pwd = req.body['pwd[]'];
+    const pwd = req.body['pwd[]']; // 参数为数组
+    // 若 tags[] 参数只有一个值，req.body['tags[]'] 会被解析为一个字符串，而不是数组。
+    // 这是由 Express 的 body - parser 或其他中间件的默认行为决定的。
+    // 当参数只有一个值时，它默认作为单个字符串处理；当有多个值时，才会作为数组处理。
+    // 这里强制使用数组
+    const tags = Array.isArray(req.body['tags[]']) ? req.body['tags[]'] : [req.body['tags[]']];
     res.set('Access-Control-Allow-Origin', '*');
     try {
         if (!check(pwd)) {
@@ -348,7 +532,7 @@ app.post('/publish', async (req, res) => {
             return;
         }
         if (type === 'blog') {
-            postBlog(uuid,title,content);
+            postBlog(uuid,title,content,tags);
         }
         // 向父进程发送消息
         // process.send({ received: true });
@@ -402,8 +586,9 @@ app.get('/blogs/get-latest-update-time', async (req, res) => {
 
 app.get('/blogs', async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
+    const tags = req.query.tags;
     try {
-        const blogs=await getBlogs();
+        const blogs=await getBlogsByTags(tags);
         res.status(200).send(blogs);
     }
     catch(error) {
@@ -434,6 +619,18 @@ app.post('/delblog', async (req, res) => {
         const { id } = req.body;
         await deleteBlogById(id);
         res.status(200).send({data: "博客删除成功",code:0});
+    }
+    catch(error) {
+        logger.info(error);
+    }
+});
+
+// 标签
+app.get('/tags', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    try {
+        const tags=await getAllTags();
+        res.status(200).send(tags);
     }
     catch(error) {
         logger.info(error);
